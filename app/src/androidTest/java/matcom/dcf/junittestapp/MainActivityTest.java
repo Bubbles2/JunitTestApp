cb2cd283fc87660f0a02b7163932e47e78977569
Donncha Finlay <dfinlay@Mini-de-Donncha.private>
2015-08-07 19:05:26 +0200

    Correct Code using Fun with Testing

--- app/src/androidTest/java/matcom/dcf/junittestapp/MainActivityTest.java
+++ app/src/androidTest/java/matcom/dcf/junittestapp/MainActivityTest.java
@@ -7,6 +7,8 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
 import android.test.UiThreadTest;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import junit.framework.TestCase;
 
@@ -15,28 +17,41 @@ import junit.framework.TestCase;
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     //
-    private MainActivity activity;
+    private MainActivity myactivity;
+    private Button mClickMeButton;
+    private TextView mFrom;
+
+    //
+
 
     //
     public MainActivityTest() {
         super(MainActivity.class);
     }
 
-
-    public void setUp() throws Exception {
+    @Override
+    protected void setUp() throws Exception {
         super.setUp();
         //
         // this must be called before getActivity()
         // disabling touch mode allows for sending key events
-        setActivityInitialTouchMode(false);
-        activity = getActivity();
+        setActivityInitialTouchMode(true);
+        // Start the main activity of the application under test
+         myactivity = getActivity();
+        //
+        //Get references to all views
+        mClickMeButton = (Button) myactivity.findViewById(R.id.btnConvert);
+        mFrom = (TextView) myactivity.findViewById(R.id.txtFrom);
+
     }
     //
     @UiThreadTest
     public void testLaunchingSubActivityFiresIntentAndFinishesSelf() {
-         View button = activity.findViewById(R.id.btnConvert);
-         button.performClick();
-         assertNotNull(button);
+        //Retrieve the top-level window decor view
+        final View decorView = myactivity.getWindow().getDecorView();
+
+        //Verify that the mClickMeButton is on screen
+        assertNotNull(mClickMeButton);
     }
 
 }
