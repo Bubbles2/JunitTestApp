f0e4462d22cbc1f16342617f12eaf22fa063a630
Donncha Finlay <dfinlay@Mini-de-Donncha.private>
2015-08-31 14:39:45 +0200

    Add View assertions margins , gravity etc....

--- app/src/androidTest/java/matcom/dcf/junittestapp/Ju4.java
+++ app/src/androidTest/java/matcom/dcf/junittestapp/Ju4.java
@@ -61,7 +61,7 @@ public class Ju4  {
 //           boolean success = fldr.mkdir();
 //       }
 
-       String file = saveToInternalSorage(screenshot);
+       //String file = saveToInternalSorage(screenshot);
        //
         //Retrieve the top-level window decor view
         final View decorView = main.get().getWindow().getDecorView();
@@ -70,25 +70,25 @@ public class Ju4  {
        Spoon.screenshot(main.get(), "initial_state");
         assertNotNull(mClickMeButton);
     }
-    private String saveToInternalSorage(Bitmap bitmapImage){
-        ContextWrapper cw = new ContextWrapper(main.get());
-        // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File mypath=new File(directory,"profile.jpg");
-
-        FileOutputStream fos = null;
-        try {
-
-            fos = new FileOutputStream(mypath);
-
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return directory.getAbsolutePath();
-    }
+//    private String saveToInternalSorage(Bitmap bitmapImage){
+//        ContextWrapper cw = new ContextWrapper(main.get());
+//        // path to /data/data/yourapp/app_data/imageDir
+//        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+//        // Create imageDir
+//        File mypath=new File(directory,"profile.jpg");
+//
+//        FileOutputStream fos = null;
+//        try {
+//
+//            fos = new FileOutputStream(mypath);
+//
+//            // Use the compress method on the BitMap object to write image to the OutputStream
+//            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+//            fos.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return directory.getAbsolutePath();
+//    }
 
 }
--- app/src/androidTest/java/matcom/dcf/junittestapp/MainActivityTest.java
+++ app/src/androidTest/java/matcom/dcf/junittestapp/MainActivityTest.java
@@ -1,16 +1,22 @@
 package matcom.dcf.junittestapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
 import android.test.UiThreadTest;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import junit.framework.TestCase;
+
+import static android.test.ViewAsserts.assertLeftAligned;
+import static android.test.ViewAsserts.assertOnScreen;
 
 /**
  * Created by dfinlay on 07/08/15.
@@ -20,6 +26,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     private MainActivity myactivity;
     private Button mClickMeButton;
     private TextView mFrom;
+    private TextView mTo;
 
     //
 
@@ -42,9 +49,90 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         //Get references to all views
         mClickMeButton = (Button) myactivity.findViewById(R.id.btnConvert);
         mFrom = (TextView) myactivity.findViewById(R.id.txtFrom);
+        mTo =  (TextView) myactivity.findViewById(R.id.txtTo);
 
     }
+
+    /**
+     *  Basic test fields and buttons exist
+     */
+    public final void testHasInputFields() {
+        assertNotNull(mFrom);
+        assertNotNull(mTo);
+        assertNotNull(mClickMeButton);
+    }
+
+    /**
+     *  Test fields start empty
+     */
+    public void testFieldsShouldStartEmpty() {
+        assertEquals("", mFrom.getText().toString());
+        assertEquals("", mTo.getText().toString());
+    }
+
+    /**
+     * Test fields are screen
+     * Should have import static android.test.ViewAsserts.assertOnScreen;
+     */
+    public void testFieldsOnScreen() {
+        View origin = myactivity.getWindow().getDecorView();
+        assertOnScreen(origin, mFrom);
+        assertOnScreen(origin, mTo);
+    }
+
+    /**
+     *  Test fields are aligned
+     */
+    public void testAlignment() {
+        assertLeftAligned(mFrom, mTo);
+    }
     //
+
+
+    /**
+     *  Test that from field is MATCH_Parent as per spec
+     */
+    public void testFromInputFieldCoversEntireScreen() {
+        ViewGroup.LayoutParams lp;
+        int expected = RelativeLayout.LayoutParams.MATCH_PARENT;
+        lp = mFrom.getLayoutParams();
+        assertEquals("From width is not MATCH_PARENT",
+                expected, lp.width);
+    }
+
+    /**
+     *  Test font size is correct
+     */
+    public void testFontSizes() {
+        float pixelSize = 36f;
+        assertEquals(pixelSize, mFrom.getTextSize());
+        assertEquals(pixelSize, mTo.getTextSize());
+////        // use the sp value in the test
+//        float pixelSize = getFloatPixelSize(R.dimen.label_text_size);
+//        assertEquals(pixelSize, mFrom.getTextSize());
+//        assertEquals(pixelSize, mTo.getTextSize());
+    }
+
+    /**
+     *  Test margins
+     */
+    public void testFromInputMargins() {
+        RelativeLayout.LayoutParams lp =
+                (RelativeLayout.LayoutParams) mFrom.getLayoutParams();
+        assertEquals(74, lp.leftMargin);
+    }
+    //
+
+    /**
+     *  Test Gravity has been correctly set
+     */
+    public void testFromInputJustification() {
+        int expectedGravity = Gravity.END | Gravity.CENTER_VERTICAL;
+        int actual = mFrom.getGravity();
+        String errorMessage = String.format(
+                "Expected 0x%02x but was 0x%02x", expectedGravity, actual);
+        assertEquals(errorMessage, expectedGravity, actual);
+    }
     @UiThreadTest
     public void testLaunchingSubActivityFiresIntentAndFinishesSelf() {
         //Retrieve the top-level window decor view
